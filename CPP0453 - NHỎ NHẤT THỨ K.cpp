fdeb50e8385a9c587b66b9f0b885d7c39d0bae4f
vietphappro27 <140454779+vietphappro27@users.noreply.github.com>
2023-10-18 07:48:39 +0700

    Create CPP0453 - NHỎ NHẤT THỨ K.cpp

---
+++ CPP0453 - NHỎ NHẤT THỨ K.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a,a+n);
+        cout << a[x-1];
+        cout << "\n";
+    }
+}
